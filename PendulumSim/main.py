fdd4ca4b4a815a42555ee3bcd1e43d7a1e8284ed
Krzysztof <krzysztof211@gmail.com>
2018-01-23 10:54:42 +0100

    Pendulum

--- PendulumSim/main.py
+++ PendulumSim/main.py
@@ -1,15 +1,50 @@
-from math import sqrt
+#import
+from math import sin
+from matplotlib import pyplot
 
-Q1 =[]
-t = []
-h1 = [0.,]
-h2 = []
-A1 = 1
-A2 = 2
+#basic variables and starting values
+x=0
+x1 =0
+dt=0.05
+theta = 90
+theta1 = 0
+g = 9.98
+oTime = 10
+l=0.5
+
+#solution arrays
+tabX=[]
+tabTheta=[]
+tabTime=[]
+
+#number of iterations
+iteration = oTime/dt
+iteration = int(iteration)
 
 
-def function(value):
+time = 0
+#iterating loop
+for i in range(iteration):
+    tabTime.append(time)
+    time+=dt
+    x1 = (-1*g*sin(theta)*dt)/l + x
+    theta1 = x*dt + theta
+    tabX.append(x1)
+    tabTheta.append(theta1)
+    x= x1
+    theta = theta1
 
+#for i in range(iteration):
+#    print(tabX[i])
+#    print("   ")
+#    print(tabTheta[i])
+#    print("\n")
 
+pyplot.plot(tabTime,tabX)
+pyplot.show()
 
-    function(5)
+pyplot.plot(tabTime,tabTheta)
+pyplot.show()
+
+pyplot.plot(tabX,tabTheta)
+pyplot.show()
